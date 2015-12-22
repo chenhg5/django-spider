78b3820e39ec7bdff9588171b38d4faa6bdd7a50
陈弘桂 <chg80333@163.com>
2015-12-22 13:20:34 +0800

    setting & view up

--- get_data/views.py
+++ get_data/views.py
@@ -1,9 +1,13 @@
 #coding:utf-8
 from django.http import HttpResponse
+from django.shortcuts import render
 
-def index(request):
-    return HttpResponse(u"Welcome")
+# def index(request):
+#     return HttpResponse(u"Welcome")
 
 def add(request):
 	c = int(request.GET['a'])+int(request.GET['b'])
 	return HttpResponse(str(c))
+
+def index(request):
+    return render(request, 'home.html')
--- get_the_data/settings.py
+++ get_the_data/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'get_data',
 )
 
 MIDDLEWARE_CLASSES = (
